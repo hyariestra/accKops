eb5cc0a0a55f923e5f07dc9674f4842c9c42ac25
hyariestra <yudhaariestra@gmail.com>
2019-03-28 00:08:07 +0700

    kode akun view

--- application/modules/pengaturan/views/pengaturan.php
+++ application/modules/pengaturan/views/pengaturan.php
@@ -1,3 +1,4 @@
+
 <!-- data pelanggan ada di var $pelanggan -->
 
 <div class="row">
@@ -10,33 +11,76 @@
 
 				<h3 class="box-title"><?php echo $judul ?></h3>
 
+				<hr/>
+
 			</div>
 
 			<div class="box-body">
 
-				<form action="pengaturan_submit" method="get" accept-charset="utf-8">
-					<div class="box box-form-md">
-          <div class="box-header with-border">
-            <h3 class="box-title">Informasi Umum</h3>
-          </div>
-          <div class="box-body">
-            
-            <div class="form-group">
-              <label for="" class="col-sm-4 control-label">Tahun<span style ="color: red"> *</span></label>
-              <div class="col-sm-8">
-                <input type="text" name="tahun" class="form-control">
-              </div>
-            </div>
-          </div>
-        </div>
-				</form>
+				
+				<div class="row">
 
+					<div class="col-md-6">
+
+						<div class="form-group">
+						    <label for="NamaPerusahaan">Nama Perusahaan</label>
+						    <input type="text" class="form-control" id="NamaPerusahaan" aria-describedby="NamaPerusahaan" placeholder="Masukan Nama Perusahaan">
+					  	</div>
+
+					  	<div class="form-group">
+						    <label for="LogoPerusahaan">Logo Perusahaan</label>
+						    <input type="file" id="LogoPerusahaan" aria-describedby="LogoPerusahaan">
+						    <small id="emailHelp" class="form-text text-muted">* Logo Perusahaan yang akan ditampilkan pada laporan keuangan</small>
+					  	</div>
+
+					  	<hr/>
+
+					    <div class="form-group">
+						    <label for="AlamatPerusahaan">Alamat</label>
+						    <textarea class="form-control" id="AlamatPerusahaan" placeholder="Masukan Alamat Perusahaan"></textarea>
+					    </div>
+
+					    <div class="form-group">
+						    <label for="KodePos">Kode Pos</label>
+						    <input type="text" class="form-control" id="KodePos" aria-describedby="KodePos" placeholder="Masukan Kode Pos Perusahaan">
+					    </div>
+	
+					</div>
+
+					<div class="col-md-6">
+
+					    <div class="form-group">
+						    <label for="WebPerusahaan">Website</label>
+						    <input type="text" class="form-control" id="WebPerusahaan" aria-describedby="WebPerusahaan" placeholder="Masukan Nama Website Perusahaan">
+					    </div>
+
+					    <div class="form-group">
+						    <label for="AlamatPerusahaan">Email</label>
+						    <input type="email" class="form-control" id="EmailPerusahaan" aria-describedby="EmailPerusahaan" placeholder="Masukan Email Perusahaan">
+					    </div>
+
+					    <hr/>
+
+					    <div class="form-group">
+						    <label for="NoTelp">Nomor Telpon</label>
+						    <input type="text" class="form-control" id="NoTelp" aria-describedby="NoTelp" placeholder="Masukan Nomor Telpon Perusahaan">
+					    </div>
+
+					    <div class="form-group">
+						    <label for="AlamatPerusahaan">Fax</label>
+						    <input type="text" class="form-control" id="NoFax" aria-describedby="NoFax" placeholder="Masukan Nomor Fax Perusahaan">
+					    </div>
+
+					</div>
+					
+				</div>
+				
 
 			</div>
 
 			<div class="box-footer">
 
-				<a href="<?php echo base_url("admin/pengaturan/tambah") ?>" class="btn btn-primary">Tambah data</a>
+				<a href="" class="btn btn-primary pull-right">Simpan</a>
 
 				
 
